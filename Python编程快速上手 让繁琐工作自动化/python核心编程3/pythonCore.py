5199df197338ba19c3ba6936c679b8e82e5c7cd3
封晔 <578797371@qq.com>
2018-02-24 17:13:59 +0800

    50

---
+++ Python编程快速上手 让繁琐工作自动化/python核心编程3/pythonCore.py
@@ -0,0 +1,218 @@
+# python3
+# pythonCore.py python核心编程
+
+import re
+
+class pythonCore:
+
+    def __init__(self):
+        pass
+
+    def testPrint(self):
+        print('test ok')
+
+    #1.3.4 使用match()方法匹配字符串
+    def testMatch(self):
+        m = re.match('foo', 'foo')
+        if m is not None:
+            print(m)
+            print(m.group())
+
+    def testMatch1(self):
+        res = re.match('foo', 'foo on the table').group()
+        print(res)
+
+    #1.3.5使用search()在一个字符串中查找模式（搜索与匹配的对比）
+    def testSearch(self):
+        m = re.search('foo', 'seafood')
+        if m is not None:
+            print(m.group())
+    
+    #1.3.6匹配多个字符串
+    def testMatchMany(self):
+        bt = 'bat|net|bit'
+        m = re.match(bt, 'bat')
+        if m is not None:
+            print(m.group())
+
+        m = re.match(bt, 'He bit me!')
+        if m is not None:
+            print(m.group())
+        else:
+            print('not match')
+            
+        m = re.search(bt, 'He bit me!')
+        if m is not None:
+            print(m.group())
+
+    #1.3.7 匹配任何单个字符
+    def matchAnySingleStr(self):
+        anyend = '.end'
+        m = re.match(anyend, 'bend')
+        if m is not None:
+            print(m.group())
+        
+        m = re.match(anyend, 'end') #不匹配任何字符
+        if m is not None:
+            print(m.group())
+        else:
+            print('None')
+
+        m = re.match(anyend, '\nend') #除了\n之外的任何字符
+        if m is not None:
+            print(m.group())
+        else:
+            print('None')
+        
+        m = re.search('.end', 'The end.') #在搜索中匹配' '
+        if m is not None:
+            print(m.group())
+        else:
+            print('None')
+
+        m = re.search('3\.14', '3.14') #匹配小数点
+        if m is not None:
+            print(m.group())
+        else:
+            print('not point')
+
+        test314 = '3.14'
+        m = re.match(test314, '3014') #点号匹配0
+        if m is not None:
+            print(m.group())
+        else:
+            print('None')
+
+        m = re.match(test314, '3.14') #点号匹配'.'
+        if m is not None:
+            print(m.group())
+        else:
+            print('None')
+
+    #1.3.8 创建字符集([])
+    def testCreateStrings(self):
+        m = re.match('[cr][23][dp][o2]', 'c3po') #匹配c3po
+        if m is not None:
+            print(m.group())
+        
+        m = re.match('r2d2|c3po', 'c2do')
+        if m is not None:
+            print(m.group())
+        else:
+            print('None')
+
+        m = re.match('r2d2|c3po', 'r2d2')
+        if m is not None:
+            print(m.group())
+        else:
+            print('None')
+        
+    #1.3.9 重复、特殊字符以及分组
+    def testRepetSpecialGroup(self):
+        patt = '\w+@\w+\.com'
+        m = re.match(patt, '123@qq.com')
+        if m is not None:
+            print(m.group())
+        else:
+            print('None')
+
+        exPatt = '\w+@(\w+\.)?\w+\.com'
+        m = re.match(exPatt, '123@www.xxx.com')
+        if m is not None:
+            print(m.group())
+        else:
+            print('None')     
+
+        exPatt1 = '\w+@(\w+\.)*\w+\.com'
+        m = re.match(exPatt1, '123@www.qqqq.ccc.com')
+        if m is not None:
+            print(m.group())
+
+        m = re.match('\w\w\w-\d\d\d', 'abc-123')
+        if m is not None:
+            print(m.group())
+
+        m = re.match('(\w\w\w)-(\d\d\d)', 'abc-123')
+        if m is not None:
+            print(m.groups())
+            print(m.group())
+            print(m.group(1))
+            print(m.group(2))
+
+    def testRepetSpecialGroup1(self):
+        m = re.match('ab', 'ab')
+        print(m.group())
+        print(m.groups())
+
+        m = re.match('(ab)', 'ab')
+        print(m.group())
+        print(m.group(1))
+        print(m.groups())
+
+        print('\n')
+        m = re.match('(a(b))', 'ab')
+        print(m.group())
+        print(m.group(1))
+        print(m.group(2))
+        print(m.groups())
+
+    #1.3.10 匹配字符串的起始和结尾及单词边界
+    def testMatchStringStartAndEnd(self):
+        m = re.search('^The', 'The end.')
+        if m is not None:
+            print(m.group())
+
+        print('在边界')
+        m = re.search(r'\bthe', 'bitethe dog')
+        if m is not None:
+            print(m.group())
+
+        m = re.search(r'\Bthe', 'bitethe dog')
+        if m is not None:
+            print(m.group())
+
+    #1.3.11 使用 findall()和 finditer()查找每一次出现的位置
+    def testFindallAndfinditer(self):
+        m = re.findall('car', 'carry the barcardi to the car')
+        print(m)
+        print('findall')
+        s = 'This and that.'
+        m = re.findall(r'(th\w+) and (th\w+)', s, re.I)
+        print(m)
+        print('finditer')
+        m = re.finditer(r'(th\w+) and (th\w+)', s, re.I)
+        for i in m:
+            print(i.group())
+
+    #1.3.12 使用sub()和subn()搜索与替换
+    def testSubAndSubn(self):
+        oldStr = 'attn:X\n\nDear X, \n'
+        print(oldStr)
+        m = re.sub('X', 'Mr. Smith', oldStr)
+        print(m)
+        m = re.subn('X', 'Mr. Smith', oldStr)
+        print(m)
+        m = re.sub(r'(\d{1,2})/(\d{1,2})/(\d{2}|\d{4})',r'\2/\1/\3', '2/20/91')
+        print(m)
+
+    #1.3.13 在限定模式上使用 split()分隔字符串
+    def testSplit(self):
+        DATA = {
+            'Mountain View, CA 94040',
+            'Sounnyvalue, CA',
+            'Cupertino 95012',
+            'Palo Alto CA',
+        }
+        for datum in DATA:
+            print(re.split(', |(?= (?:\d{5}|[A-Z]{2}))', datum))
+
+    #1.3.14 扩展符号
+    def testExtendSymbol(self):
+        
+        
+
+        
+#启动函数           
+if __name__ == '__main__':
+    pc = pythonCore()
+    pc.testSplit()
