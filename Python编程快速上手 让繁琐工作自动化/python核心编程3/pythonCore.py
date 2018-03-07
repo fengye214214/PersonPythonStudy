adbbcf9f7b4c186ca1fb63cc14a307788272970c
封晔 <578797371@qq.com>
2018-03-07 10:47:43 +0800

    59

--- Python编程快速上手 让繁琐工作自动化/python核心编程3/pythonCore.py
+++ Python编程快速上手 让繁琐工作自动化/python核心编程3/pythonCore.py
@@ -2,6 +2,7 @@
 # pythonCore.py python核心编程
 
 import re
+import os
 
 class pythonCore:
 
@@ -208,12 +209,64 @@ class pythonCore:
 
     #1.3.14 扩展符号
     def testExtendSymbol(self):
-        pass
+        res = re.findall(r'(?i)yes','yes? Yes. YES!!')
+        print(res)
+        res = re.findall(r'(?i)th\w', 'The quickest way is through this tuunnel.')
+        print(res)
+        res = re.findall(r'(?im)(^th[\w ]+)', """
+                        This line is the first,
+                        another line,
+                        that line, it's the best
+                        """)
+        print(res)
+        res = re.findall(r'th.+', '''
+                        The first line
+                        the second line
+                        the third line
+                        ''')
+        print(res)
+        res = re.findall(r'(?s)th.+','''
+                        The first line
+                        the second line
+                        the third line
+                        ''')
+        print('1' + str(res))
+        res = re.search(r'''(?x)
+                    \((\d{3})\) #区号
+                    [ ] #空白符
+                    (\d{3}) #前缀
+                    -      #横线
+                    (\d{4}) #终点数字
+                    ''', '(800) 555-1212').groups()
+        print(res)
+        res = re.findall(r'http://(?:\w+\.)*(\w+\.com)',
+                        'http://google.com http://www.google.com http://code.google.com')
+        print(res)
+        res = re.search(r'\((?P<areacode>\d{3})\) (?P<prefix>\d{3})-(?:\d{4})', '(800) 555-1212').groupdict()
+        print(res)
+        res = re.findall(r'\w+(?= van Rossum)',
+                        '''
+                        Guido van Rossum
+                        Tim Pters
+                        Alex Martelli
+                        Just van Rossum
+                        Raymoud Hettinger
+                        ''')
+        print(res)
         
         
+    def reWho(self):
+        with os.popen('whodata.txt', 'r') as f:
+            for eachLine in f:
+                print(re.split(r'\s\s+', eachLine.strip()))
+    
+    def retasklist(self):
+        with open('ta.txt', 'r') as f:
+            for eachLine in f:
+                print(re.split(r'\s\s+|t', eachLine.strip()))
 
         
 #启动函数           
 if __name__ == '__main__':
     pc = pythonCore()
-    pc.testSplit()
+    pc.reWho()
